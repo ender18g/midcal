ceb88630a88f8b0b0a0d94254de860a933aa1e80
Allan <allan.elsberry@gmail.com>
2023-11-10 08:07:52 -0500

    increased title size

--- src/TitleBar.jsx
+++ src/TitleBar.jsx
@@ -7,7 +7,7 @@ export default function TitleBar() {
 		<Flex px={5} py={2} justify={'space-between'} alignItems={'center'} bg={'blue.700'} boxShadow={'lg'}>
 			<Box w={'100vw'} textAlign={'center'}>
 				<Flex justify={'center'} align={'center'}>
-					<Heading letterSpacing={7} fontWeight={100} fontSize={'3xl'} color={'white'}>
+					<Heading letterSpacing={7} fontWeight={200} fontSize={'4xl'} color={'white'}>
 						<CalIcon />
 						MidCal
 					</Heading>
@@ -22,7 +22,7 @@ export default function TitleBar() {
 			</Box>
 			<Link href="https://www.usna.edu/Academics/Calendars-Events/index.php" isExternal>
 				<Flex textAlign={'center'} justifyContent={'center'} align={'center'} flexWrap={'wrap'}>
-					<Image src={crest} alt={'USNA Crest'} w={'2em'} />
+					<Image src={crest} alt={'USNA Crest'} w={'2.5em'} />
 				</Flex>
 			</Link>
 		</Flex>
